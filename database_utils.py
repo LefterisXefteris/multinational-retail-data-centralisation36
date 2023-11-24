e17af1f0b90ecdb03d07b50488cf06a99863c280
Lefteris Gilmaz <lefterisgilmaz@lefteriss-mbp.speedport.ip>
2023-11-24 14:41:01 +0100

    creates the database init function

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,5 @@
 import yaml 
+from sqlalchemy import create_engine
 
 class DatabaseConnector:
     
@@ -6,3 +7,32 @@ class DatabaseConnector:
         with open("credentials.yaml", "r") as f:
             credentials = yaml.safe_load(f)
         return credentials
+    
+    def init_db_engine():
+        creds = read_db_creds()
+
+        conn_str = f"{creds['driver']}://{creds['user']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['database']}"
+
+        engine = sqlalchemy.create_engine(conn_str)
+        return engine
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ss = DatabaseConnector()
+sa = ss.read_db_creds
+print(sa)
