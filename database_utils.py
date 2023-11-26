d8105da26a6529fb5cdca116b4d53a64cf3882db
Lefteris Gilmaz <lefterisgilmaz@Lefteriss-MacBook-Pro.fritz.box>
2023-11-26 13:02:11 +0100

    implements the init_db_engine function

--- database_utils.py
+++ database_utils.py
@@ -1,19 +1,19 @@
+import sqlalchemy
 import yaml 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, engine_from_config, text
 
 class DatabaseConnector:
     
-    def read_db_creds():
+
+    def read_db_creds(self):
         with open("credentials.yaml", "r") as f:
             credentials = yaml.safe_load(f)
         return credentials
     
-    def init_db_engine():
-        creds = read_db_creds()
-
-        conn_str = f"{creds['driver']}://{creds['user']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['database']}"
-
-        engine = sqlalchemy.create_engine(conn_str)
+    def init_db_engine(self):
+        creds = self.read_db_creds()
+        con_str = f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
+        engine = sqlalchemy.create_engine(con_str)
         return engine
 
 
@@ -21,18 +21,3 @@ class DatabaseConnector:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-ss = DatabaseConnector()
-sa = ss.read_db_creds
-print(sa)
