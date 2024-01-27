1d1e1a591cc55ab6e234c76c836985763b947d44
Lefteris Gilmaz <lefterisgilmaz@Lefteriss-MacBook-Pro.local>
2024-01-27 21:29:56 +0000

    lilo

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,16 +9,19 @@ class DataCleaning:
     def clean_user_data(self, table):
         clean_user_data = table.drop_duplicates(subset=['email_address', 'address', 'phone_number', 'user_uuid'], keep='last').reset_index(drop=True)
         clean_user_data = table.loc[table['country_code'].str.len() <= 2]
+        
 
+        # WILL REVISE THIS METHOD LATER
         return clean_user_data
 
 
     def clean_card_data(self, table):
-
         table['card_number']= table['card_number'].astype(str).apply(lambda x: x.strip('?') if '?' else x)
-        
+        table['card_number'] = table['card_number'].str.replace(r'\D', '', regex=True)
         return table 
-       
+    
+    def called_clean_store_data(self):
+        pass
 
 
         
--- data_extraction.py
+++ data_extraction.py
@@ -1,3 +1,5 @@
+import requests
+from urllib import request, response
 from PyPDF2 import PdfFileReader
 import pandas as pd
 
@@ -13,8 +15,32 @@ class DataExtractor:
             print("Failed to read the tables", e)
 
     
+    def list_number_of_stores(self, header, url):
+        response = requests.get(url, headers = header)
+        json_re = response.json()
+        return json_re['number_stores']
+
+    def retrieve_stores_data(self, url, header):
+            
+        list_of_frames = []
+        for i in range(452):
+            response = requests.get(f'{url}{i}',headers=self.header)
+            list_of_frames.append(pd.json_normalize(response.json()))
+        return pd.concat(list_of_frames)
+
+
+
+
+
+
+
 
 
     
 
+
+
+
+
+    
 
